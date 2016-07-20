290b9e2a8df8449ee4ab384b6304de4387f26e98
IgorDonizetti <igordonizetti75@hotmail.com>
2016-07-19 21:22:33 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<meta charset="utf-8">
+<head>
+	<title>Perguntas e respostas</title>
+	<link rel="stylesheet" type="text/css" href="css/indexcss.css">
+</head>
+<body>
+  <header>
+   <h1>Quiz Perguntas e Respostas</h1>
+  </header>
+<section>
+<h2>Quiz de Conhecimentos Gerais.</h2>
+
+<form method="get" action="resposta.php">
+<fieldset>
+<legend>(01) Número que precede uma expressão</legend>
+<input type="radio" name="resposta1" id="numerador"/><label for="numerador">Numerador</label><br>
+<input type="radio" name="resposta2" id="multiplicador"/><label for="multiplicador">Multiplicador</label><br>
+<input type="radio" name="resposta3" id="coeficiente"/><label for="coeficiente">Coeficiente</label>
+</fieldset>
+
+<fieldset>
+<legend>(02) Número de constituições que o Brasil já teve, incluindo a atual</legend>
+<input type="radio" name="resposta4" id="num5"/><label for="num5">5</label><br>
+<input type="radio" name="resposta5" id="num7"/><label for="num7">7</label><br>
+<input type="radio" name="resposta6" id="num8"/><label for="num8">8</label>
+</fieldset>
+
+<fieldset>
+<legend>(03) Aquele que executa, ou dirige a execução, da impressão de textos</legend>
+<input type="radio" name="resposta7" id="Tipografo"/><label for="Tipografo">Tipógrafo</label><br>
+<input type="radio" name="resposta8" id="Tipografico"/><label for="Tipografico">Tipografico</label><br>
+<input type="radio" name="resposta9" id="Cartografo"/><label for="Cartografo">Cartógrafo</label>
+</fieldset>
+
+<fieldset>
+<legend>(04) Vegetação rasteira encontrada próximo às regiões polares</legend>
+<input type="radio" name="resposta10" id="Coniferas"/><label for="Coniferas">Coniferas</label><br>
+<input type="radio" name="resposta11" id="Tundra"/><label for="Tundra">Tundra</label><br>
+<input type="radio" name="resposta12" id="Gramineas"/><label for="Gramineas">Gramíneas</label>
+</fieldset>
+
+<fieldset>
+<legend>(05) Número de estados da Região Sudeste do Brasil</legend>
+<input type="radio" name="resposta13" id="alt6"/><label for="alt6">6</label><br>
+<input type="radio" name="resposta14" id="alt5"/><label for="alt5">5</label><br>
+<input type="radio" name="resposta15" id="alt3"/><label for="alt3">3</label>
+</fieldset>
+
+<input id="botao" type="submit" name="Enviar">
+</form>
+</section>
+
+<!--COMENTARIO-RESPOSTAS
+1 - Coeficiente
+2 - 7
+3 - Tipógrafo
+4 - Tundra
+5 - 6
+-->
+</body>
+</html>
---
+++ resposta.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+$pergunta1 = isset($_GET['resposta3']);
+$pergunta2 = isset($_GET["resposta5"]);
+$pergunta3 = isset($_GET["resposta8"]);
+$pergunta4 = isset($_GET["resposta11"]);
+$pergunta5 = isset($_GET["resposta13"]);
+$pontuacao = 0;
+
+if ($pergunta1 == 'resposta3') {
+	$mensagemr1 = "Resposta correta";
+    $pontuacao = $pontuacao+20 ;
+}
+else {
+	$mensagemr1 = "Resposta Errada";
+}
+
+echo "1-$mensagemr1<br>";
+
+if ($pergunta2 == 'resposta5') {
+	$mensagemr2 = "Resposta correta";
+    $pontuacao = $pontuacao+20 ;
+}
+else {
+	$mensagemr2 = "Resposta Errada";
+}
+
+echo "2-$mensagemr2<br>";
+
+if ($pergunta3 == 'resposta8') {
+	$mensagemr3 = "Resposta correta";
+    $pontuacao = $pontuacao+20 ;
+}
+else {
+	$mensagemr3 = "Resposta Errada";
+}
+
+echo "3-$mensagemr3<br>";
+
+if ($pergunta4 == 'resposta11') {
+	$mensagemr4 = "Resposta correta";
+    $pontuacao = $pontuacao+20 ;
+}
+else {
+	$mensagemr4 = "Resposta Errada";
+}
+
+echo "4-$mensagemr4<br>";
+
+if ($pergunta5 == 'resposta13') {
+	$mensagemr5 = "Resposta correta";
+    $pontuacao = $pontuacao+20 ;
+}
+else {
+	$mensagemr5 = "Resposta Errada";
+}
+
+echo "5-$mensagemr5<br>";
+echo "Pontuação : $pontuacao"	
+?>
+</body>
+</html>
